d8566ddc3b42059bdb0627db9507e022ce95c77d
AlexSmakhtin <lokomotiv96000@gmail.com>
2023-06-21 19:38:47 +0300

    init change index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,37 @@
 <?php
 
-echo "Hello world";
+//echo "Hello world";
 
+//$variable = "John";
+//$vard = "Surname";
+//echo "<br>";
+
+//echo $variable;
+//echo "<br>";
+//echo $variable.$vard;
+//echo "<br>";
+
+//$array = array(
+//'key' =>'value',
+//'key1' =>'value1',
+
+//);
+
+//print_r($array);
+//echo "<br>";
+
+//echo "<br>";
+//foreach($array as $key => $value){
+//echo "$key $value<br>";
+//}
+//echo "<pre> ";
+//print_r($_GET);
+//echo "<pre> ";
+//vivod parametrov get zaprosa
+
+//echo "
+//<form method= 'post'>
+//<input type='text'>
+//<br>
+//<input type='submit' value='submit'>
+//<form>";
